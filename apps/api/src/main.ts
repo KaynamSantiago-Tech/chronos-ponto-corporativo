401a6aa9a9e2397c60c3ce0063c56877a0736a47
KaynamSantiago-Tech <kaynam.tecnologia@gmail.com>
2026-04-17 08:47:19 -0300

    Auto-update: 2026-04-17 08:47:19

---
+++ apps/api/src/main.ts
@@ -0,0 +1,48 @@
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from "helmet";
+import { Logger } from "nestjs-pino";
+
+import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
+  app.use(helmet());
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(",") ?? ["http://localhost:3000"],
+    credentials: true,
+  });
+  app.setGlobalPrefix("");
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const swagger = new DocumentBuilder()
+    .setTitle("Midrah Ponto API")
+    .setDescription("API interna de ponto corporativo")
+    .setVersion("0.1.0")
+    .addBearerAuth()
+    .build();
+  SwaggerModule.setup("docs", app, SwaggerModule.createDocument(app, swagger));
+
+  const port = Number(process.env.PORT ?? 3333);
+  await app.listen(port, "0.0.0.0");
+  // eslint-disable-next-line no-console
+  console.log(`[api] ready on :${port}`);
+}
+
+bootstrap().catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error("bootstrap failed", err);
+  process.exit(1);
+});
